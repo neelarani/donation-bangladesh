b3f86e54519272ba0cc5f4da087ca1d3990a9779
Nila Rani <neela9624@gmail.com>
2024-09-23 19:26:24 +0600

    added functionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,38 @@
-document.getElementById('donate-btn').addEventListener('click', function () {
-  const inputField = parseFloat(document.getElementById('input-field').value);
+function donationButtons(buttonId, inputFieldId, balanceId) {
+  document.getElementById(buttonId).addEventListener('click', function () {
+    const inputAmount = parseFloat(document.getElementById(inputFieldId).value);
 
-  if (isNaN(inputField) || inputField <= 0) {
-    alert('Please enter a valid donation amount.');
-    return;
-  }
+    if (isNaN(inputAmount) || inputAmount <= 0) {
+      alert('Please enter a valid donation amount.');
+      return;
+    }
 
-  const donateMoney = parseFloat(
-    document.getElementById('donate-money').innerText
-  );
+    const donateMoney = parseFloat(
+      document.getElementById('donate-money').innerText
+    );
 
-  if (inputField > donateMoney) {
-    alert('The donation amount is more then the balance.');
-  }
+    if (inputAmount > donateMoney) {
+      alert('The donation amount is more than the balance.');
+      const modal = document.getElementById('my_modal_1');
+      modal.close();
+      return;
+    }
 
-  document.getElementById('balance').innerText = `${inputField}`;
+    const currentBalance = parseFloat(
+      document.getElementById(balanceId).innerText
+    );
+    document.getElementById(balanceId).innerText = `${
+      currentBalance + inputAmount
+    }`;
 
-  const newDonateMoney = donateMoney - inputField;
-  const updateMoney = (document.getElementById('donate-money').innerText =
-    newDonateMoney);
+    const newDonateMoney = donateMoney - inputAmount;
+    document.getElementById('donate-money').innerText = newDonateMoney;
 
-  const modal = document.getElementById('my_modal_1');
-  modal.showModal();
-});
+    const modal = document.getElementById('my_modal_1');
+    modal.showModal();
+  });
+}
+
+donationButtons('donate-btn-1', 'input-field-1', 'balance-1');
+donationButtons('donate-btn-2', 'input-field-2', 'balance-2');
+donationButtons('donate-btn-3', 'input-field-3', 'balance-3');
