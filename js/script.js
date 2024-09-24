e98feea56a8c3a099769f07850f73bf09fa16722
Nila Rani <neela9624@gmail.com>
2024-09-24 12:45:09 +0600

    added buttons handle function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+// function toggleHistory() {
+//   const mainContent = document.getElementById('main-content');
+//   const donationData = document.getElementById('donation-data');
+//   if (donationData.classList.contains('hidden')) {
+//     donationData.classList.remove('hidden');
+//     mainContent.classList.add('hidden');
+//   } else {
+//     donationData.classList.add('hidden');
+//     mainContent.classList.remove('hidden');
+//   }
+// }
+
+// function addDonation(donationId, inputAmount) {
+//   const donatationData = document.getElementById('donation-data');
+
+//   const createElement = document.createElement('div');
+//   const donateLocation = document.getElementById(donationId).innerText;
+
+//   createElement.className = 'border-2 shadow-sm p-9 space-y-4 rounded-lg';
+//   createElement.innerHTML = `
+//     <h3>${donateLocation}</h3>
+//         <p class="text-lg font-medium text-text ">${inputAmount}<span class="pl-2">BDT</span> </p>
+//         <p class="text-lg font-medium text-text">${new Date().toLocaleDateString()}</p>
+//   `;
+//   donatationData.appendChild(createElement);
+
+//   toggleHistory();
+// }
+
 function donationButtons(buttonId, inputFieldId, balanceId) {
   document.getElementById(buttonId).addEventListener('click', function () {
     const inputAmount = parseFloat(document.getElementById(inputFieldId).value);
@@ -30,6 +59,10 @@ function donationButtons(buttonId, inputFieldId, balanceId) {
 
     const modal = document.getElementById('my_modal_1');
     modal.showModal();
+
+    // addDonation('donate-noakhali', inputAmount);
+    // addDonation('donate-feni', inputAmount);
+    // addDonation('donate-protest', inputAmount);
   });
 }
 
@@ -37,5 +70,34 @@ donationButtons('donate-btn-1', 'input-field-1', 'balance-1');
 donationButtons('donate-btn-2', 'input-field-2', 'balance-2');
 donationButtons('donate-btn-3', 'input-field-3', 'balance-3');
 
-// **********
-const donationButton = document.getElementById('donation-button');
+// Donation and history buttons handle function
+const donationBtn = document.getElementById('donation-button');
+const historyButton = document.getElementById('history-button');
+const mainContent = document.getElementById('main-content');
+const donationData = document.getElementById('donation-data');
+
+function showDonation() {
+  mainContent.classList.remove('hidden');
+  donationData.classList.add('hidden');
+
+  donationBtn.classList.add('bg-secondary');
+  historyButton.classList.remove('bg-secondary');
+
+  historyButton.classList.add('bg-gray-300');
+  donationBtn.classList.remove('bg-gray-300');
+}
+
+function showHistory() {
+  mainContent.classList.add('hidden');
+  donationData.classList.remove('hidden');
+
+  historyButton.classList.add('bg-secondary');
+  donationBtn.classList.remove('bg-secondary');
+
+  donationBtn.classList.add('bg-gray-300');
+  historyButton.classList.remove('bg-gray-300');
+}
+donationBtn.classList.add('bg-secondary');
+
+donationBtn.addEventListener('click', showDonation);
+historyButton.addEventListener('click', showHistory);
